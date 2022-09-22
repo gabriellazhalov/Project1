f8401dde4c491ea1c20ea666adb0bf030f37bbbc
Gabriella Zhalov <zhalovgv@clarkson.edu>
2022-09-22 08:56:38 -0400

    MessageClackData v.2 Constructors created

--- src/MessageClackData.java
+++ src/MessageClackData.java
@@ -1,2 +1,13 @@
-public class MessageClackData {
+public abstract class MessageClackData extends ClackData {
+    private String message;
+
+    public MessageClackData(String userName, String message, int type){
+        super(userName, type);
+        this.message = message;
+    }
+
+    public MessageClackData() {
+        this("Anon", "N/A", 0);
+    }
+
 }
