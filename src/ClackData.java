79b2f924e8a435fbebee226d0ba251595d518aa6
Gabriella Zhalov <zhalovgv@clarkson.edu>
2022-09-22 08:48:49 -0400

    ClackData v.2 most methods implemented MessageClackData v.1 class created

--- src/ClackData.java
+++ src/ClackData.java
@@ -1,3 +1,37 @@
-public class ClackData {
-    public String username;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+public abstract class ClackData {
+    private String userName;
+    private int type;
+    private java.time.LocalDate date;
+
+    public ClackData(String userName, int type) {
+        this.userName = userName;
+        this.type = type;
+        date = java.time.LocalDate.now();
+    }
+
+    public ClackData(int type) {
+        this("Anon",type);
+    }
+
+    public ClackData() {
+        this(0);
+    }
+
+    public int getType() {
+        return type;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public java.time.LocalDate getDate() {
+        return date;
+    }
+
+    //Abstract Method
+    public abstract MessageClackData getData();
 }
