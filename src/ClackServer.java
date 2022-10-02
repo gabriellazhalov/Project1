742815c7663e032ac565b935aed96f4740f400aa
Aengus Andrew <aengusandrew215@gmail.com>
2022-10-01 21:29:01 -0400

    ClackServer v.1 Class methods written and implemented

---
+++ src/ClackServer.java
@@ -0,0 +1,57 @@
+public class ClackServer {
+    private int port;
+    private boolean closeConnection;
+    private ClackData dataToReceiveFromClient;
+    private ClackData dataToSendToClient;
+
+    private final int DEFAULT_PORT = 7000;
+
+    public ClackServer(int port) {
+        this.port = port;
+        dataToReceiveFromClient = null;
+        dataToSendToClient = null;
+    }
+
+    public ClackServer() {
+        this(7000);
+    }
+
+    public void start() {
+
+    }
+
+    public void receiveData() {
+
+    }
+
+    public void sendData() {
+
+    }
+
+    public int getPort() {
+        return getPort();
+    }
+
+    public int hashCode() {
+        int hashedValue = 0;
+        hashedValue += this.port;
+        hashedValue += dataToReceiveFromClient.hashCode();
+        hashedValue += dataToSendToClient.hashCode();
+        if(closeConnection) hashedValue += 1;
+        return hashedValue;
+    }
+
+    public boolean equals(Object obj) {
+        ClackServer clackServer = (ClackServer) obj;
+        return this.port == clackServer.port &&
+                this.closeConnection == clackServer.closeConnection &&
+                this.dataToReceiveFromClient.equals(clackServer.dataToReceiveFromClient) &&
+                this.dataToSendToClient.equals(clackServer.dataToSendToClient);
+    }
+
+    public String toString() {
+        return ("Port Number: " + this.port + "\nClose Connection: " + closeConnection + "\nData to send to Client: " + dataToSendToClient + "\nData to receive from Client" + dataToReceiveFromClient);
+    }
+
+
+}
